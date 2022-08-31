25bb7526a3f214c9302f90aeb06929bb42328806
hyeyoung.dev <hyeyoung.dev@gmail.com>
2022-08-31 14:50:49 +0900

    vue 프로젝트 커밋

---
+++ vue-project/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+     news: []
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
